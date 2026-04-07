403b428a081933db37bf697df2ba778c2166bbcb
matsumotodmclinic <waiwai7077@gmail.com>
2026-04-07 22:02:13 +0900

    Update auth.js

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
   }
 
   const { password } = req.body;
-  const correct = process.env.APP_SHARED_PASSWORD;
+  const correct = '1234';
 
   if (!correct) {
     return res.status(500).json({ error: 'パスワードが設定されていません' });
